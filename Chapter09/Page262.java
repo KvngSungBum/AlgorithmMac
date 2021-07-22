cbb65a5a0c9f6f79987d09b55bedd4b56b9322c3
KangSungBum <peter9706@naver.com>
2021-07-22 14:01:38 +0900

    Chapter09

---
+++ Chapter09/Page262.java
@@ -0,0 +1,19 @@
+package Chapter09;
+
+import java.util.*;
+
+public class Page262 {
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int node = sc.nextInt();
+        int edge = sc.nextInt();
+        int dest = sc.nextInt();
+        int[][] map = new int[node][node]; //노드
+
+        for(int i=0;i<edge;i++){
+            
+            
+        }
+    }
+}
