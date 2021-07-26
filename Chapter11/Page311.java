05bb09ee584d4b6495d002f248b8f68d51559964
KangSungBum <peter9706@naver.com>
2021-07-26 17:08:14 +0900

    Chapter11

---
+++ Chapter11/Page311.java
@@ -0,0 +1,34 @@
+package Chapter11;
+
+import java.util.*;
+
+public class Page311 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] fear = new int[n];
+
+        for(int i=0;i<n;i++){
+            fear[i] = sc.nextInt();
+        }
+        for(int i=0;i<fear.length;i++){
+            for(int j=0;j<fear.length-i-1;j++){
+                if(fear[j]>fear[j+1]){
+                    int temp = fear[j+1];
+                    fear[j+1] = fear[j];
+                    fear[j]=temp;
+                }
+            }
+        }
+        int remainder =0;int result =0;
+        for(int i=0;i<n;i++){
+            remainder ++;
+            if(fear[i] == remainder){
+                result++;
+                remainder=0;
+            }
+        }
+        System.out.println(result);
+    }
+
+}
