a8a4544f5f4e0a5eee4adb2fdd557feb8909c42e
KangSungBum <peter9706@naver.com>
2021-07-29 17:10:45 +0900

    Problem Solved

---
+++ Chapter11/Page312.java
@@ -0,0 +1,40 @@
+package Chapter11;
+
+import java.util.Scanner;
+
+public class Page312 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String number = sc.nextLine();
+        sc.close();
+
+        char[] number2 = new char[number.length()];
+        int[] arr = new int[number.length()];
+
+        //입력받은 문자열 int형 배열로
+        number2 = number.toCharArray();
+        for(int i=0;i<number2.length;i++){
+            arr[i] = number2[i] -'0';
+        }
+        
+        int result = calculate(arr);
+        System.out.println(result);
+
+    }
+
+    private static int calculate(int[] arr) {
+        int result = arr[0];
+        for(int i=0;i<arr.length-1;i++){
+            if(arr[i] == 0){
+                result = result + arr[i + 1];
+            }else{
+                if(arr[i+1]==0){
+                    result = result + arr[i+1];
+                }else{
+                    result = result * arr[i + 1];
+                }
+            }
+        }
+        return result;
+    }
+}
