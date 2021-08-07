5d53d588b9e83afff3a258e2d5eea683eef1ca0b
KangSungBum <peter9706@naver.com>
2021-08-07 21:03:35 +0900

    Skeleton Code

---
+++ Chapter11/Page316.java
@@ -0,0 +1,14 @@
+package Chapter11;
+
+import java.util.*;
+
+public class Page316 {
+    public int solution(int[] foot_times, long k){
+        int answer=0;
+        return answer;
+    }
+
+    public static void main(String[] args){
+
+    }
+}
