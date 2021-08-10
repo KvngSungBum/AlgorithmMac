e3a195df551b5fc05a8b8b9d8c1e5d27af2dc272
KangSungBum <peter9706@naver.com>
2021-08-10 14:05:21 +0900

    return -1 added

--- Chapter11/Page316.java
+++ Chapter11/Page316.java
@@ -6,19 +6,24 @@ public class Page316 {
     public static int solution(int[] food_times, long k){
         int answer = 0;
         int status=0;
+        int flag =0;
 
-        while(status ==k){
+        while(status == k){
             if(food_times[answer]!=0){
                 food_times[answer] --;
+                if(food_times[answer]==0){
+                    flag++;
+                }
                 answer = (answer+1)% food_times.length;
                 status++;
             }
-            else{
+            else if(food_times[answer]==0){
                 answer = (answer+1)% food_times.length;
                 status++;
             }
-
         }
+        if(flag == food_times.length)
+            answer=-1;
         answer = answer+1;
         return answer;
     }
@@ -36,6 +41,5 @@ public class Page316 {
 
         int result = solution(food_times, k);
         System.out.println(result);
-
     }
 }
