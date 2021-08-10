d8539c4585808366e1bd59c1e7985c9f2ee5e382
KangSungBum <peter9706@naver.com>
2021-08-10 12:34:57 +0900

    Solved but failed in test case

--- Chapter11/Page316.java
+++ Chapter11/Page316.java
@@ -3,12 +3,39 @@ package Chapter11;
 import java.util.*;
 
 public class Page316 {
-    public int solution(int[] foot_times, long k){
-        int answer=0;
+    public static int solution(int[] food_times, long k){
+        int answer = 0;
+        int status=0;
+
+        while(status ==k){
+            if(food_times[answer]!=0){
+                food_times[answer] --;
+                answer = (answer+1)% food_times.length;
+                status++;
+            }
+            else{
+                answer = (answer+1)% food_times.length;
+                status++;
+            }
+
+        }
+        answer = answer+1;
         return answer;
     }
 
     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int foodNum = sc.nextInt();
+        long k = sc.nextLong();
+
+        int[] food_times = new int[foodNum];
+
+        for(int i=0;i<foodNum;i++){
+            food_times[i] = sc.nextInt();
+        }
+
+        int result = solution(food_times, k);
+        System.out.println(result);
 
     }
 }
