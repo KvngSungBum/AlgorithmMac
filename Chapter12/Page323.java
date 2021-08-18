a65b152a59860e324f75a08424c2542d2490c681
KangSungBum <peter9706@naver.com>
2021-08-18 18:10:54 +0900

    Problem Solving

--- Chapter12/Page323.java
+++ Chapter12/Page323.java
@@ -5,43 +5,29 @@ import java.util.Scanner;
 
 public class Page323 {
     public static int solution(String s){
-        int answer = 0;
-        int[] shortest = new int[s.length()];
+        int answer =s.length();
 
-        for(int cutter=1;cutter<=s.length();cutter++){
-            int result =0;
-            int temp_count=1;
+        for(int size =1;size<=s.length()/2;size++){
             StringBuilder compressedStr = new StringBuilder();
             String temp1 = "";
             String temp2 = "";
-            int end=0;
-            for(int index=0;(index+cutter+cutter)<=cutter*(s.length()/cutter);index+=cutter){
-                temp1 = s.substring(index,index+cutter);
-                temp2 = s.substring(index+cutter, index+cutter+cutter);
-                if(temp1.equals(temp2)){
+            int limit = s.length() - (s.length()%size);
+            int temp_count=1;
+
+            for(int i=0;i+(2*size)<=limit;i+=size){
+                temp1 =s.substring(i,i+size);
+                temp2 = s.substring(i+size, i+(2*size));
+
+                if (temp1.equals(temp2)) {
                     temp_count++;
-                }
-                else {
-                    if (temp_count != 1)
-                        compressedStr.append(temp_count);
+                }else{
+                    compressedStr.append(temp_count);
                     compressedStr.append(temp1);
-                    temp_count = 1;
+                    temp_count=1;
                 }
-                end = index;
             }
-            compressedStr.append(s.substring(end));
-            result = compressedStr.length();
-            shortest[cutter-1] = result;
         }
 
-        Arrays.sort(shortest);
-//        for(int i=0;i< shortest.length;i++){
-//            if(shortest[i]!=0){
-//                answer = shortest[i];
-//                break;
-//            }
-//        }
-        answer = shortest[0];
         return answer;
     }
 
