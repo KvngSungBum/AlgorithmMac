26ad28a2a4ece99c38103640176c447ef16e00a2
KangSungBum <peter9706@naver.com>
2021-08-19 18:43:15 +0900

    Problem Solving

--- Chapter12/Page323.java
+++ Chapter12/Page323.java
@@ -1,33 +1,51 @@
 package Chapter12;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class Page323 {
     public static int solution(String s){
-        int answer =s.length();
-
-        for(int size =1;size<=s.length()/2;size++){
+        int answer = s.length();
+        for(int size = 1; size<=s.length()/2;size++){
             StringBuilder compressedStr = new StringBuilder();
+            int limit = s.length() - (s.length()%size);
+            int temp_count =1;
+            int remainder_count=1;
+            int result =0;
+            int indexStatus =0;
             String temp1 = "";
             String temp2 = "";
-            int limit = s.length() - (s.length()%size);
-            int temp_count=1;
+            boolean contains_num = false;
+            ArrayList<String> same = new ArrayList<>();
 
-            for(int i=0;i+(2*size)<=limit;i+=size){
-                temp1 =s.substring(i,i+size);
-                temp2 = s.substring(i+size, i+(2*size));
-
-                if (temp1.equals(temp2)) {
-                    temp_count++;
-                }else{
-                    compressedStr.append(temp_count);
-                    compressedStr.append(temp1);
-                    temp_count=1;
+            if(s.substring(0,size).equals(s.substring(size,size+size))){//0부터 같아야지만 진행
+                String temp="";
+                for(int i=0;i+size<=s.length();i+=size){
+                    same.add(s.substring(i, i + size));
                 }
+                temp=same.get(0);
+                for(int i=1;i<same.size();i++){
+                    if (same.get(i).equals(temp)) {
+                        temp_count++;
+                    }else{
+                        if(temp_count!=1)
+                            compressedStr.append(temp_count);
+                        compressedStr.append(same.get(i));
+                        temp_count=1;
+                    }
+                    temp=same.get(i);
+                }
+                System.out.println(compressedStr);
+                answer = Math.min(answer, compressedStr.length());
             }
-        }
 
+//            System.out.println(size);
+//            for(int i=0;i<same.size();i++){
+//                System.out.print(same.get(i)+" ");
+//            }
+//            System.out.println();
+        }
         return answer;
     }
 
