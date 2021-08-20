37ddb49124db46a8c42eea31df37ad9d50a87093
KangSungBum <peter9706@naver.com>
2021-08-20 14:01:20 +0900

    Problem Solved: Answer Code

--- Chapter12/Page323.java
+++ Chapter12/Page323.java
@@ -7,44 +7,31 @@ import java.util.Scanner;
 public class Page323 {
     public static int solution(String s){
         int answer = s.length();
-        for(int size = 1; size<=s.length()/2;size++){
-            StringBuilder compressedStr = new StringBuilder();
-            int limit = s.length() - (s.length()%size);
+        for(int size = 1; size<s.length()/2 +1;size++){
+            String compressed = "";
+            String prev = s.substring(0,size);
             int temp_count =1;
-            int remainder_count=1;
-            int result =0;
-            int indexStatus =0;
-            String temp1 = "";
-            String temp2 = "";
-            boolean contains_num = false;
-            ArrayList<String> same = new ArrayList<>();
 
-            if(s.substring(0,size).equals(s.substring(size,size+size))){//0부터 같아야지만 진행
-                String temp="";
-                for(int i=0;i+size<=s.length();i+=size){
-                    same.add(s.substring(i, i + size));
-                }
-                temp=same.get(0);
-                for(int i=1;i<same.size();i++){
-                    if (same.get(i).equals(temp)) {
-                        temp_count++;
-                    }else{
-                        if(temp_count!=1)
-                            compressedStr.append(temp_count);
-                        compressedStr.append(same.get(i));
-                        temp_count=1;
+            for(int index = size;index<s.length();index+=size){
+                String sub = "";
+                for(int k= index; k< index + size;k+=size){
+                    if (k < s.length()) {
+                        sub += s.charAt(k);
                     }
-                    temp=same.get(i);
                 }
-                System.out.println(compressedStr);
-                answer = Math.min(answer, compressedStr.length());
+                if(prev.equals(sub)) temp_count++;
+                else{
+                    compressed += (temp_count>=2) ? temp_count +prev : prev;
+                    sub="";
+                    for(int k=index;k<index+size;k++){
+                        if(k<s.length()) sub += s.charAt(k);
+                    }
+                    prev = sub;
+                    temp_count=1;
+                }
             }
-
-//            System.out.println(size);
-//            for(int i=0;i<same.size();i++){
-//                System.out.print(same.get(i)+" ");
-//            }
-//            System.out.println();
+            compressed += (temp_count >= 2) ? temp_count + prev : prev;
+            answer = Math.min(answer, compressed.length());
         }
         return answer;
     }
