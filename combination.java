d1c264e84e007785733dca96ba095291804ea3bd
KangSungBum <peter9706@naver.com>
2021-08-28 01:13:19 +0900

    Combination: Skeleton Code

---
+++ combination.java
@@ -0,0 +1,23 @@
+public class combination {
+    static void combination(int[] arr, boolean[] visited, int start, int n, int r) {
+        if (r == 0) {
+            print(arr, visited, n);
+            return;
+        }
+
+        for (int i = start; i < n; i++) {
+            visited[i] = true;
+            combination(arr, visited, i + 1, n, r - 1);
+            visited[i] = false;
+        }
+    }
+
+    static void print(int[] arr, boolean[] visited, int n) {
+        for (int i = 0; i < n; i++) {
+            if (visited[i]) {
+                System.out.print(arr[i] + " ");
+            }
+        }
+        System.out.println();
+    }
+}
