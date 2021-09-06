ee07dd7ecc2c3aba9f2c3bc1d54ce8770e66d888
KangSungBum <peter9706@naver.com>
2021-09-06 17:32:32 +0900

    Permutation: Skeleton Code

---
+++ permutation.java
@@ -0,0 +1,26 @@
+public class permutation {
+    static void permutation(int[] arr, int depth, int n, int r) {
+        if (depth == r) {
+            print(arr, r);
+            return;
+        }
+
+        for (int i=depth; i<n; i++) {
+            swap(arr, depth, i);
+            permutation(arr, depth + 1, n, r);
+            swap(arr, depth, i);
+        }
+    }
+
+    static void swap(int[] arr, int depth, int i) {
+        int temp = arr[depth];
+        arr[depth] = arr[i];
+        arr[i] = temp;
+    }
+
+    static void print(int[] arr, int r) {
+        for (int i = 0; i < r; i++)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+}
