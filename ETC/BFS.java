3bfb66a55c2cafa5133f358c8de0d971dd79765d
KangSungBum <peter9706@naver.com>
2021-09-16 21:25:06 +0900

    BFS Skeleton Code

---
+++ ETC/BFS.java
@@ -0,0 +1,29 @@
+package ETC;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BFS {
+    public static boolean[] visited = new boolean[9];
+    public static ArrayList<ArrayList<Integer>> graph = new ArrayList<ArrayList<Integer>>();
+
+    public static void bfs(int start) {
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(start);
+
+        visited[start] = true;
+
+        while (!q.isEmpty()) {
+            int x = q.poll();
+            System.out.print(x + " ");
+            for(int i=0;i<graph.get(x).size();i++){
+                int y = graph.get(x).get(i);
+                if (!visited[y]) {
+                    q.offer(y);
+                    visited[y] = true;
+                }
+            }
+        }
+    }
+}
